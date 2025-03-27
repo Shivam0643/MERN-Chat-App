570509c4504d3bec917c5126dbe63612d1aedb24
Shivam0643 <shivamlashkari246@gmail.com>
2025-03-27 10:26:10 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -11,6 +12,8 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -27,10 +30,11 @@ app.use("/api/auth",authRoutes) //from req.body
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
-// app.get('/', (req, res) => {
-//     // root routes http://localhost:5000/
-//     res.send('Hello World! how are you ');
-// })
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 
 server.listen(PORT, () => {
